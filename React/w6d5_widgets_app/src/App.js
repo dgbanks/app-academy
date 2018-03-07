05aeb160af981c2e20f999cf678b233eaf58598a
David <davidgbanks91@gmail.com>
2018-03-07 15:20:41 -0800

    Widgets project tabs

--- React/w6d5_widgets_app/src/App.js
+++ React/w6d5_widgets_app/src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Clock from './clock';
+import Tabs from './tabs';
+import Weather from './weather';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Clock />
+        <Tabs tabs={[
+          {title: 'One', content: 'something'},
+          {title: 'Two', content: 'anything'},
+          {title: 'Three', content: 'everything'}
+        ]}/>
+        <Weather />
       </div>
     );
   }
---
+++ React/w6d5_widgets_app/src/tabs.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Tabs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selectedIndex: 0 };
+  }
+
+  render() {
+    return (
+      <div style={{border: "1px solid red"}}>
+        <ul style={{
+          display:"flex",
+          justifyContent: "space-between",
+          border:"1px solid orange"
+        }}>
+          {
+            this.props.tabs.map((tab, i) => {
+              return (
+                <div
+                  style={{border: "1px solid yellow"}}
+                  onClick={() => this.setState({selectedIndex: i})}>
+                  {tab.title}
+                </div>
+              );
+            })
+          }
+        </ul>
+        <article style={{border: "1px solid green"}}>
+          <h1>{this.props.tabs[this.state.selectedIndex].content}</h1>
+        </article>
+      </div>
+    );
+  }
+}
+
+export default Tabs;
