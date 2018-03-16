fa01d1fa199ccf1aa66a21c8feab483c88f6394a
David <davidgbanks91@gmail.com>
2018-03-15 19:06:50 -0700

    User, Artwork, ArtworkShare controllers

--- Rails/W4D1_project/app/controllers/users_controller.rb
+++ Rails/W4D1_project/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def create
-    user = User.new(params.require(:user).permit(:username))
+    user = User.new(user_params)
     if user.save
       render json: user
     else
@@ -9,9 +9,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    render json: user
+  end
+
   def index
     users = User.all
     render json: users
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
